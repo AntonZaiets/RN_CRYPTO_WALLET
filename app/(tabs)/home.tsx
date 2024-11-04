ee755d48a04ad3735d57ac2e005238d498c80d29
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-11-04 21:56:16 +0200

    fix

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Text } from 'react-native'
+import {Text, View} from 'react-native'
 const Home = () => {
     return(
-        <>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
             <Text>Home</Text>
-        </>
+        </View>
     );
 }
 
