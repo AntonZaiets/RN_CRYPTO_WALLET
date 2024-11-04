084fd02d71db4d387b44a06399572c886e79274e
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-11-04 21:24:57 +0200

    route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Stack.Screen name="home" />
     </Stack>
   );
 }
+
+export default RootLayout
